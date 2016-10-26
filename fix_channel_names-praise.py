7a82e735c2aa3f8de9a3e47f38ec3d5e71b5c6ae
Rae Lasko <rlasko@cmu.edu>
2016-10-26 13:00:02 -0400

    added file to format files with no annotator in column title

---
+++ fix_channel_names-praise.py
@@ -0,0 +1,85 @@
+# For praise files with time stamp and all mix-se and mix-sd files
+# remove annotator name from channel headings, duplicate channels and add tutee and tutor labels
+
+import os
+import csv
+import copy
+
+def run(origin):
+    num = 1
+    if (not os.path.isdir(origin)):
+        print("Please input folder name")
+        return
+    print("Finding files...")
+    dest = origin + "/corrected_channels"
+    if (not os.path.exists(dest)): os.mkdir(dest)
+    for filename in os.listdir(origin):
+        if (os.path.isdir(origin + "/" + filename)): continue
+        print("Processing file " + str(num))
+        get_channels(origin + "/" + filename, dest)
+        num += 1
+    print("Done!")
+
+def get_channels(origin,dest):
+    # variable index locations
+    timeIndex = 2 # index location of time column
+    channel_start = 5 # index location of first annotation column
+
+    if (os.path.isdir(origin) or not origin.endswith(".csv")): return
+    base = os.path.splitext(os.path.basename(origin))[0]
+    filenameP = dest + "/" + base + "_channelsP"+ ".csv"
+    filenameT1 = dest + "/" + base + "_channelsT1"+ ".csv"
+    filenameT2 = dest + "/" + base + "_channelsT2"+ ".csv"
+
+    reader = csv.reader(open(origin))
+    header = next(reader)
+    channel_end = len(header)
+
+    with open(filenameP, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(header)
+        for row in reader:
+            writer.writerow(row)
+
+    # Modify header for T1 and T2
+    headerT1 = copy.deepcopy(header)
+    headerT2 = copy.deepcopy(header)
+    for i in range(channel_start,channel_end):
+        if (headerT1[i][-2:len(headerT1[i])] == "P1"):
+            headerT1[i] = headerT1[i][:-2] + "TUTOR"
+            headerT2[i] = headerT2[i][:-2] + "TUTEE"
+        else:
+            headerT1[i] = headerT1[i][:-2] + "TUTEE"
+            headerT2[i] = headerT2[i][:-2] + "TUTOR"
+
+    reader = csv.reader(open(origin))
+    next(reader)
+    # write T1 file
+    with open(filenameT1, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(headerT2)
+        start = False
+        for row in reader:
+            if "T1" in row[timeIndex]:
+                start = True
+            elif row[timeIndex] != "": start = False
+
+            if start: writer.writerow(row)
+
+    reader = csv.reader(open(origin))
+    next(reader)
+
+    #write T2 file
+    with open(filenameT2, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(headerT2)
+        for row in reader:
+            if "T2" in row[timeIndex]:
+                start = True
+            elif row[timeIndex] != "": start = False
+
+            if start: writer.writerow(row)
+
+
+
+run("/Users/RaeLasko/Documents/CMU/ArticuLab/TAR source files/mix-sd/converted/column_fix")
---
+++ fix_channel_names_mixed_columns.py
@@ -0,0 +1,61 @@
+# For files that don't separate column by p1/p2
+# Create separate columns
+
+import os
+import csv
+import copy
+
+def run(origin):
+    num = 1
+    if (not os.path.isdir(origin)):
+        print("Please input folder name")
+        return
+    print("Finding files...")
+    dest = origin + "/column_fix"
+    if (not os.path.exists(dest)): os.mkdir(dest)
+    for filename in os.listdir(origin):
+        if (os.path.isdir(origin + "/" + filename)): continue
+        print("Processing file " + str(num))
+        get_channels(origin + "/" + filename, dest)
+        num += 1
+    print("Done!")
+
+def get_channels(origin,dest):
+    # variable index locations
+    timeIndex = 2 # index location of time column
+    channel_start = 5 # index location of first annotation column
+    p2_transcript_index = 4
+
+    if (os.path.isdir(origin) or not origin.endswith(".csv")): return
+    base = os.path.splitext(os.path.basename(origin))[0]
+    filenameP = dest + "/" + base + "_columnfix"+ ".csv"
+
+    reader = csv.reader(open(origin))
+    header = next(reader)
+    channel_end = len(header)
+    end_of_unique = channel_end
+    num_of_unique_channels = channel_end - channel_start
+
+    # remove annotator name
+    for i in range(channel_start, channel_end):
+        remove = header[i][::-1].find("_")
+        remove = len(header[i]) - remove - 1
+        header[i] = header[i][:remove]
+
+    # create p1/p2 header
+    header = header + copy.deepcopy(header[channel_start:]) # add missing columns
+    channel_end = len(header) # update end location
+    for i in range(channel_start,channel_end):
+        header[i] += "_P1" if i < end_of_unique else "_P2"
+
+    with open(filenameP, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(header)
+        for row in reader:
+            newRow = row + [""] * num_of_unique_channels
+            if (newRow[p2_transcript_index]): # P2 said something, need to switch annotation location
+                newRow[end_of_unique:] = copy.deepcopy(newRow[channel_start:end_of_unique]) # copy into new location
+                newRow[channel_start:end_of_unique] = [""] * num_of_unique_channels # delete from old location
+            writer.writerow(newRow)
+
+run("/Users/RaeLasko/Documents/CMU/ArticuLab/TAR source files/mix-sd/converted")
