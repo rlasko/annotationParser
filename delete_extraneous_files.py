8697effcffd0a07a66c7a886dc910279958c9158
Rae Lasko <rlasko@cmu.edu>
2016-11-10 20:19:05 -0500

    Delete files with p1 p2 channels

---
+++ delete_extraneous_files.py
@@ -0,0 +1,14 @@
+import os
+
+def delete_extraneous_files(origin):
+    if (os.path.isdir(origin) == False):
+        if (origin.endswith(".csv")): # make sure it's a csv
+            # delete
+            filename = os.path.splitext(os.path.basename(origin))[0]
+            if "channelsP" in filename:
+                os.remove(origin)
+    else:
+        for filename in os.listdir(origin):
+            delete_extraneous_files(origin + "/" + filename)
+
+delete_extraneous_files("/Users/RaeLasko/Documents/CMU/ArticuLab/File cleaning/Filtered Channels T1:T2 Only")
